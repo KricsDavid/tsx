e1bb07c008fe86f51c49a35e9e42887b2bcc0eb6
KricsDavid <david@kricsfalusi.hu>
2024-09-18 09:07:11 +0200

    lll

--- vite-project/src/UserDetails.tsx
+++ vite-project/src/UserDetails.tsx
@@ -3,12 +3,14 @@ export function UserDetails() {
 const details:string="Ez a user details komponens"
 const age:number = 26
 const isOnline:Boolean =false
+let array:string[] = ["pizza", "Hamburger", "Shake"]
 
 return(
 <div>
 <h1> {details}</h1>
 <h1>{age}</h1>
 <h1>{isOnline ? "Online" : "Nincs online"}</h1>
+<h1>{array}</h1>
 
 </div>
 
